68507ee9a16c2351cdac8103faff95ebd2721626
achmadirfanda199 <achmadirfanda199@gmail.com>
2023-07-02 21:55:57 +0700

    test commit1

---
+++ build/app/generated/source/buildConfig/debug/com/example/irfanda_mobile_fe_ordo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.irfanda_mobile_fe_ordo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.irfanda_mobile_fe_ordo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
